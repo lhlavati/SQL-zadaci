3b801bb56ea6ea5e89ba0b45a67766ce11f75673
lhlavati <lukahlavati@gmail.com>
2019-05-27 23:37:45 +0200

    Insert i update sa operatorima

--- SQL/NHL.sql
+++ SQL/NHL.sql
@@ -5,7 +5,7 @@ create database NHL;
 use NHL;
 
 CREATE TABLE tim(
-	sifra INT PRIMARY KEY NOT NULL,
+	sifra INT PRIMARY KEY NOT NULL auto_increment,
 	ime VARCHAR(100) NOT NULL,
 	grad VARCHAR(50),
 	trener VARCHAR(50),
@@ -13,7 +13,7 @@ CREATE TABLE tim(
 );
 
 CREATE TABLE igrac(
-	sifra INT PRIMARY KEY NOT NULL,
+	sifra INT PRIMARY KEY NOT NULL auto_increment,
 	ime VARCHAR(50),
 	prezime VARCHAR(50) NOT NULL,
 	pozicija CHAR(2) NOT NULL,
@@ -22,16 +22,16 @@ CREATE TABLE igrac(
 );
 
 CREATE TABLE utakmica(
-	sifra INT PRIMARY KEY NOT NULL,
+	sifra INT PRIMARY KEY NOT NULL auto_increment,
 	rezultatDomacin INT,    
 	rezultatGost INT,    
-	datumUtakmice DATETIME NOT NULL,
+	datumUtakmice DATETIME DEFAULT NOW() NOT NULL,
 	domacin INT,
 	gost INT
 );
 
 CREATE TABLE ozljeda(
-	sifra INT PRIMARY KEY NOT NULL,
+	sifra INT PRIMARY KEY NOT NULL auto_increment,
 	opisIncidenta VARCHAR (200),
 	opisOzljede VARCHAR (200) NOT NULL,
 	igrac INT
@@ -42,3 +42,30 @@ ALTER TABLE igrac ADD FOREIGN KEY (tim) REFERENCES tim(sifra);
 ALTER TABLE utakmica ADD FOREIGN KEY (domacin) REFERENCES tim(sifra);
 ALTER TABLE utakmica ADD FOREIGN KEY (gost) REFERENCES tim(sifra);
 ALTER TABLE ozljeda ADD FOREIGN KEY (igrac) REFERENCES igrac(sifra);
+
+INSERT INTO tim (sifra, ime)
+VALUES (1, 'New York Rangers');
+
+INSERT INTO tim (sifra, ime)
+VALUES (2, 'Vegas Golden Knights');
+
+INSERT INTO igrac (sifra, prezime, pozicija, brojDresa, tim)
+VALUES (1, 'Andersson', 'C', 50, 1);
+
+INSERT INTO igrac (sifra, prezime, pozicija, brojDresa, tim)
+VALUES (2, 'Carrier', 'LW', 28, 2);
+
+INSERT INTO utakmica (sifra, datumUtakmice, domacin, gost)
+VALUES (1, default, 1, 2);
+
+INSERT INTO utakmica (sifra, datumUtakmice, domacin, gost)
+VALUES (2, '2019-05-28', 2, 1);
+
+INSERT INTO ozljeda (sifra, opisOzljede, igrac)
+VALUES (1, 'Uganuce gležnja', 1);
+
+UPDATE tim SET trener = 'David Quinn'
+WHERE NOT sifra = 2;
+
+UPDATE tim SET trener = 'Gerard Gallant'
+WHERE sifra > 1;
