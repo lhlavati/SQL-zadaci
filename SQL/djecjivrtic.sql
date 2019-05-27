5a8202ecfdc5641276cb36666ad21788d8325e2f
lhlavati <lukahlavati@gmail.com>
2019-05-27 14:48:42 +0200

    Update, insert i delete

--- SQL/djecjivrtic.sql
+++ SQL/djecjivrtic.sql
@@ -1,3 +1,6 @@
+﻿
+# d:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "D:\GitHub\SQL\djecjivrtic.sql"
+
 drop database if exists djecjivrtic;
 create database djecjivrtic;
 use djecjivrtic;
@@ -25,3 +28,42 @@ CREATE TABLE djeca(
 
 ALTER TABLE djeca ADD FOREIGN KEY (odgojnaskupina) REFERENCES odgojnaskupina(sifra);
 ALTER TABLE djeca ADD FOREIGN KEY (odgajateljica) REFERENCES odgajateljica(sifra);
+
+INSERT INTO odgojnaskupina
+VALUES (1, "Zvoncic");
+
+INSERT INTO odgojnaskupina
+VALUES (2, "Jabuka");
+
+INSERT INTO odgojnaskupina
+VALUES (3, "Jež");
+
+INSERT INTO odgajateljica (sifra, ime, prezime)
+VALUES (1, "Ivana", "Sigurnjak");
+
+INSERT INTO odgajateljica (sifra, ime, prezime)
+VALUES (2, "Miss", "Fortune");
+
+INSERT INTO djeca
+VALUES (1, "Luka", "Hlavati", 2, 1);
+
+INSERT INTO djeca
+VALUES (2, "Luka", "Hulak", 3, 2);
+
+INSERT INTO djeca
+VALUES (3, "Josip", "Josic", 1, 2);
+
+INSERT INTO djeca (sifra, ime, prezime)
+VALUES (4, "Red za", "brisanje.");
+
+DELETE FROM djeca
+WHERE sifra = 4;
+
+UPDATE odgojnaskupina SET naziv = "Breskva"
+WHERE naziv = "Jež";
+
+UPDATE odgajateljica SET ime = "Ivana", prezime = "Ivić"
+WHERE sifra = 1;
+
+UPDATE djeca SET ime = "Lux"
+WHERE ime = "Luka";
