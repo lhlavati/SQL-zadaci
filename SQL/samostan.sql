6679ce1702eb166eb6db80541600c2a40af9a33d
lhlavati <lukahlavati@gmail.com>
2019-05-28 13:51:31 +0200

    Insert into

--- SQL/samostan.sql
+++ SQL/samostan.sql
@@ -1,4 +1,4 @@
-# d:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "D:\GitHub\SQL\samostan.sql"
+﻿# d:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "D:\GitHub\SQL\samostan.sql"
 
 drop database if exists samostan;
 create database samostan DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;
@@ -9,7 +9,7 @@ CREATE TABLE svecenik(
 	ime VARCHAR(50) NOT NULL,
 	prezime VARCHAR(50),
 	OIB CHAR(11),
-	nadredeni INT
+	nadredeni INT NOT NULL
 );
 
 CREATE TABLE posao(
@@ -25,3 +25,27 @@ CREATE TABLE obavlja(
 ALTER TABLE svecenik ADD FOREIGN KEY (nadredeni) REFERENCES svecenik(sifra);
 ALTER TABLE obavlja ADD FOREIGN KEY (svecenik) REFERENCES svecenik(sifra);
 ALTER TABLE obavlja ADD FOREIGN KEY (posao) REFERENCES posao(sifra);
+
+INSERT INTO posao
+VALUES (1, 'Pranje poda');
+
+INSERT INTO posao
+VALUES (2, 'Priprema za svetu misu');
+
+INSERT INTO posao
+VALUES (3, 'Slaganje knjiga');
+
+INSERT INTO svecenik (sifra, ime, nadredeni)
+VALUES (1, 'Franjo', 1);
+
+INSERT INTO svecenik (sifra, ime, nadredeni)
+VALUES (2, 'Toni', 1);
+
+INSERT INTO svecenik (sifra, ime, nadredeni)
+VALUES (3, 'Ivan', 3);
+
+INSERT INTO obavlja
+VALUES (1, 3);
+
+INSERT INTO obavlja
+VALUES (2, 1);
